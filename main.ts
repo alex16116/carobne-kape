8e3dc5c0a3c074160c9fb6f96cebf378c522477a
RoboKlik <60173099+roboklik@users.noreply.github.com>
2020-02-21 09:08:42 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,67 @@
+input.onPinPressed(TouchPin.P1, function () {
+    radio.sendNumber(2)
+})
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendNumber(1)
+})
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("" + (imena[0]))
     pin = ""
+    desetisko = 0
+    basic.showIcon(IconNames.No)
 })
 input.onButtonPressed(Button.B, function () {
-    if (pin.length > 3) {
+    if (pin.length == 4) {
         basic.showString(pin)
         basic.showNumber(desetisko)
-    } else {
-        pin = "" + pin + "0"
-        desetisko = desetisko * 2
-    }
-})
-radio.onReceivedNumber(function (prejeto) {
-    if (prejeto == desetisko) {
-        basic.showIcon(IconNames.Happy)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    if (pin.length > 3) {
-        basic.showString(pin)
-        basic.showNumber(desetisko)
+        pin = ""
     } else {
         pin = "" + pin + "1"
         desetisko = desetisko * 2 + 1
     }
 })
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendNumber(3)
+})
+radio.onReceivedNumber(function (prejeto) {
+    if (prejeto == 1) {
+        basic.showString("" + (imena[desetisko]))
+    } else if (prejeto == 2) {
+        basic.showString("" + (crke[desetisko]))
+    } else if (prejeto == 3) {
+        while (true) {
+            images.createBigImage(`
+                # . . . # . . . # #
+                # # . . . . . # # .
+                . # # . . . # # . .
+                . . # # . # # . . .
+                . . . # # # . . . #
+                `).showImage(3)
+        }
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (pin.length == 4) {
+        basic.showString(pin)
+        basic.showNumber(desetisko)
+        pin = ""
+    } else {
+        pin = "" + pin + "0"
+        desetisko = desetisko * 2
+    }
+})
+let crke: string[] = []
 let imena: string[] = []
 let pin = ""
 let desetisko = 0
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
 radio.setGroup(21)
 desetisko = 0
 pin = ""
-imena = ["ROBOKLIK", "Kaja", "jure", "Dejan", "Klemen", "Jan", "luka", "žan", "milena", "Miha", "alex", "-", "-", "-", "-", "-"]
-basic.forever(function () {
-    basic.showString("" + (imena[desetisko]))
-})
+imena = ["ROBOKLIK", "KAJA", "JURE", "DEJAN", "KLEMEN", "JAN", "LUKA", "ŽAN", "MILENA", "MIHA", "ALEX", "X", "X", "X", "X", "X"]
+crke = ["ROBOKLIK", "R", "O", "B", "O", "K", "L", "I", "K", "*", "*", "-", "-", "-", "-", "-"]
